9a0aa5de40009d6a0b0dfe62368df360ca470e46
Jeremy Kolassa <jeremy.kolassa@aximgeo.com>
2022-07-08 17:11:24 -0400

    Learned how to display the enum value name instead of just the value itself. Also how to constrain to the enum as well. This will come in handy.

--- src/components/CurrentStage.tsx
+++ src/components/CurrentStage.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import { useState } from 'react';
+import * as React from "react";
+import { useState } from "react";
 
 enum ECurrentStage {
-  'START' = 1,
-  'MIDDLE' = 2,
-  'FINAL' = 3,
-  'REVIEW' = 4,
+  "START" = 1,
+  "MIDDLE" = 2,
+  "FINAL" = 3,
+  "REVIEW" = 4,
 }
 
 export default function CurrentStage() {
@@ -13,7 +13,9 @@ export default function CurrentStage() {
     ECurrentStage.START
   );
 
-  function increment(value: ECurrentStage) {
+  /* function increment(value: ECurrentStage) {
+    let stageName = ECurrentStage[value];
+    console.log("stageName: ", stageName);
     let result = value + 1;
     setCurrentStage(result);
   }
@@ -21,21 +23,46 @@ export default function CurrentStage() {
   function decrement(value: ECurrentStage) {
     let result = value - 1;
     setCurrentStage(result);
+  } */
+
+  function action(value: ECurrentStage, action: "increment" | "decrement") {
+    let currstageName = ECurrentStage[value];
+    console.log("currstageName ", currstageName);
+    switch (action) {
+      case "increment":
+        value++;
+        if (value > 4) {
+          return TypeError("currentStage is out of bounds");
+        } else {
+          setCurrentStage(value);
+        }
+        break;
+      case "decrement":
+        value--;
+        if (value < 1) {
+          return TypeError("currentStage is out of bounds");
+        } else {
+          setCurrentStage(value);
+        }
+        break;
+      default:
+        break;
+    }
   }
 
   return (
     <div>
-      <p>Current Stage Is: {CurrentStage}</p>
-      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+      <p>Current Stage Is: {ECurrentStage[CurrentStage]}</p>
+      <div style={{ display: "flex", justifyContent: "space-around" }}>
         <button
           className="button is-primary"
-          onClick={() => increment(CurrentStage)}
+          onClick={() => action(CurrentStage, "increment")}
         >
           Increment
         </button>
         <button
           className="button is-primary"
-          onClick={() => decrement(CurrentStage)}
+          onClick={() => action(CurrentStage, "decrement")}
         >
           Decrement
         </button>
